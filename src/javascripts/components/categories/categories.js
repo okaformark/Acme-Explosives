7eaf3940af0e35e8dac841f937a44d6c7a3ad9ff
Mark Okafor <okaformark@gmail.com>
2019-05-18 14:21:07 -0500

    products array filtered

--- src/javascripts/components/categories/categories.js
+++ src/javascripts/components/categories/categories.js
@@ -1,14 +1,15 @@
 import categoryData from '../../helpers/Data/categoriesData';
-// import typeData from '../../helpers/Data/typesData';
+import typeData from '../../helpers/Data/typesData';
+
 
 const initCategory = () => {
   categoryData.loadCategories()
     .then((response) => {
       const results = response.data.categories;
-      console.error(results);
-      // typeData.getTypesForEachCategory(results);
+      typeData.getTypesForEachCategory(results);
     })
     .catch(err => console.error(err));
 };
+
 
 export default { initCategory };
--- src/javascripts/helpers/Data/productsData.js
+++ src/javascripts/helpers/Data/productsData.js
@@ -2,4 +2,25 @@ import axios from 'axios';
 
 const loadProductData = () => axios.get('../db/products.json');
 
-export default { loadProductData };
+const getProductsForEachType = types => new Promise((resolve, reject) => {
+  console.error(typeof types);
+  axios.get('../db/products.json')
+    .then((response) => {
+      const { products } = response.data;
+      console.error(typeof products);
+      const productsWithTypes = types.map((type) => {
+        const newType = type;
+        const arrayObjects = Object.values(products[0]);
+        // console.error(arrayObjects);
+        const matchingTypes = arrayObjects.filter(product => product.type === type.id);
+        // console.error(matchingTypes);
+        newType.product = matchingTypes;
+        console.error(newType);
+        return newType;
+      });
+      resolve(productsWithTypes);
+    })
+    .catch(err => reject(err));
+});
+
+export default { getProductsForEachType, loadProductData };
